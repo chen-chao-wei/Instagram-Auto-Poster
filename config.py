ab4a645dbb4edc2adeef187986c8c3022c31a2c8
Ivan <48938797+chen-chao-wei@users.noreply.github.com>
2025-06-10 00:00:59 +0800

    Switch to dotenv config

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 import os
 from datetime import datetime
+from dotenv import load_dotenv
 
 from utils import get_unique_path
 
-CANVA_TEMPLATE_URL = "https://www.canva.com/design/DAGpqS08mgg/ETfC__j_GCjnc3h0sG2haw/edit"
-COOKIE_FILE = "canva_cookies.txt"  # 用戶的 Canva Cookie 檔案
-OUTPUT_DIR = "output"
+load_dotenv()
+
+CANVA_TEMPLATE_URL = os.getenv(
+    "CANVA_TEMPLATE_URL",
+    "https://www.canva.com/design/DAGpqS08mgg/ETfC__j_GCjnc3h0sG2haw/edit",
+)
+COOKIE_FILE = os.getenv("COOKIE_FILE", "canva_cookies.txt")
+OUTPUT_DIR = os.getenv("OUTPUT_DIR", "output")
 
 
 def get_caption_file() -> str:
